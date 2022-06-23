274bd500bd7ac0bc840c53e35d654a03ea97dd87
rrxloyeon <rrxloyeon@gmail.com>
2022-06-23 13:36:07 +0900

    run Foreign_Material & error Parasitic

--- generate_defect.py
+++ generate_defect.py
@@ -1,6 +1,7 @@
 import random
 import cv2
 import os
+import numpy as np
 import matplotlib.pyplot as plt
 from distribution import guess_background, CLASS_ORANGE, CLASS_GREEN
 from albumentations.core.transforms_interface import ImageOnlyTransform
@@ -19,13 +20,13 @@ class DefectAugmentation(ImageOnlyTransform):
         defects_folder (str): path to the folder with defects images
     """
 
-    def __init__(self, defects=2, dark_defect=False, always_apply=False, p=0.5): # p는 왜 있는거지
+    def __init__(self, defects=1, dark_defect=False, always_apply=False, p=0.5): # p는 왜 있는거지
         super().__init__(always_apply, p)
         self.defects = defects
         self.dark_defect = dark_defect
         self.defects_folder = defect_dir_args
 
-    def apply(self, background_dir=data_dir_args+"/Pass", class_name=default_class_args, **kwargs):
+    def apply(self, background_dir, class_name):
         """
         Args:
             image (PIL Image): Image to draw defects on.
@@ -38,7 +39,7 @@ class DefectAugmentation(ImageOnlyTransform):
         src_dir = os.path.join(background_dir, random.choice(background_images))
         image = cv2.cvtColor(cv2.imread(src_dir), cv2.COLOR_BGR2RGB)
         background_type = guess_background(image)
-        self.defects_folder = os.path.join(self.defects_folder, background_type, class_name)
+        defects_folder = os.path.join(self.defects_folder, background_type, class_name)
         
         n_defects = random.randint(1, self.defects) # for this example I put 1 instead of 0 to illustrate the augmentation
         
@@ -46,28 +47,33 @@ class DefectAugmentation(ImageOnlyTransform):
             return image
         
         # height, width, _ = image.shape  # target image width and height
-        defects_images = [im for im in os.listdir(self.defects_folder) if 'png' in im]
+        defects_images = [im for im in os.listdir(defects_folder) if 'png' in im]
         
         defect_dirs = []
         for _ in range(n_defects):
-            defect_dirs.append(os.path.join(self.defects_folder, random.choice(defects_images)))
+            defect_dirs.append(os.path.join(defects_folder, random.choice(defects_images)))
             defect = cv2.cvtColor(cv2.imread(defect_dirs[-1]), cv2.COLOR_BGR2RGB)
             defect = cv2.flip(defect, random.choice([-1, 0, 1]))
             defect = cv2.rotate(defect, random.choice([0, 1, 2]))
             
             # extract real defect from defect image
             img2gray = cv2.cvtColor(defect, cv2.COLOR_BGR2GRAY)
-            ret, mask = cv2.threshold(img2gray, 10, 255, cv2.THRESH_BINARY)
+            _, mask = cv2.threshold(img2gray, 254, 255, cv2.THRESH_BINARY_INV)
             mask_inv = cv2.bitwise_not(mask)
 
+            contours,hierarchy = cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+            cnt = contours[0]
+            x,y,w,h = cv2.boundingRect(cnt)
+            defect = defect[y:y+h,x:x+w]
+
             h_height, h_width, _ = defect.shape  # defect image width and height
-            roi_ho = random.randint(0, image.shape[0] - defect.shape[0])
-            roi_wo = random.randint(0, image.shape[1] -defect.shape[1])
+            roi_ho = int(random.gauss((image.shape[0] - defect.shape[0])/2, (image.shape[0] - defect.shape[0])/10))
+            roi_wo = int(random.gauss((image.shape[1] - defect.shape[1])/2, (image.shape[0] - defect.shape[0])/10))
             roi = image[roi_ho:roi_ho + h_height, roi_wo:roi_wo + h_width]
 
             # Creating a mask and inverse mask 
             img2gray = cv2.cvtColor(defect, cv2.COLOR_BGR2GRAY)
-            ret, mask = cv2.threshold(img2gray, 10, 255, cv2.THRESH_BINARY)
+            _, mask = cv2.threshold(img2gray, np.mean(defect)*0.9, 255, cv2.THRESH_BINARY_INV)
             mask_inv = cv2.bitwise_not(mask)
 
             # Now black-out the area of defect in ROI
@@ -75,7 +81,6 @@ class DefectAugmentation(ImageOnlyTransform):
 
             # Take only region of defect from defect image.
             if self.dark_defect:
-                img_bg = cv2.bitwise_and(roi, roi, mask=mask_inv)
                 defect_fg = cv2.bitwise_and(img_bg, img_bg, mask=mask)
             else:
                 defect_fg = cv2.bitwise_and(defect, defect, mask=mask)
@@ -85,4 +90,4 @@ class DefectAugmentation(ImageOnlyTransform):
 
             image[roi_ho:roi_ho + h_height, roi_wo:roi_wo + h_width] = dst
                 
-        return image, src_dir, defect_dirs
+        return image, src_dir, defect_dirs, background_type
--- main.py
+++ main.py
@@ -1,16 +1,61 @@
 from generate_defect import DefectAugmentation
 import matplotlib.pyplot as plt
+from glob import glob
+import pickle5
+import cv2
+import os
+from tqdm import tqdm
+
+PRINT_LOG = True
 
 src_dir_args = "/home/esoc/datasets/Bulryang_12inch/Pass"
-save_fig_name = "test.jpg"
+save_dir_args = "/home/esoc/koosy/git_rrxloyeon/gen_defect_dataset/madebyme"
+save_log_args = True
 
-aug = DefectAugmentation()
-image, src_dir, defects_dir = aug.apply()
+def make_name(classname, num, dir=save_dir_args):
+    # make save_fig_name
+    return "{}/{}/{}.jpg".format(dir, classname, num)
 
-print("koo test : src_dir :", src_dir)
-print("koo test : defects_dir :")
-for d in defects_dir :
-    print(d)
-plt.imshow(image)
-plt.savefig(save_fig_name)
-print("Success saving image..", save_fig_name)
+def perform_augmentation():
+    aug = DefectAugmentation()
+    log = {'src_dir' : [], 'defects_dir' : [], 'output_dir':[], 'background_type' : [], 'src_save_dir' : []}
+    classes = ['Foreign_Material', 'Parasitic']
+    for c in classes :
+        files = glob("/home/esoc/koosy/git_rrxloyeon/gen_defect_dataset/original/"+c+"/*")
+        # num_files = len([im for im in files if 'jpg' in im])
+        num_files = 10
+        pbar = tqdm(range(num_files))
+        for i in pbar:
+            save_fig_name = make_name(c, i)
+            image, src_dir, defects_dir, bg_type = aug.apply(src_dir_args, c)
+            log['src_dir'].append(src_dir)
+            log['defects_dir'].append(defects_dir)
+            log['output_dir'].append(save_fig_name)
+            log['background_type'].append(bg_type)
+
+            # save figure
+            plt.imshow(image)
+            if not os.path.isdir(os.path.split(save_fig_name)[0]):
+                os.makedirs(os.path.split(save_fig_name)[0])
+            plt.savefig(save_fig_name)
+            if PRINT_LOG :
+                # print("Success saving image..", c, "{}/{}".format(i, num_files))
+                pbar.set_description("Saving Image.. " + c)
+
+            # save figure : Pass(src)
+            pass_dir = os.path.join(save_dir_args, 'Pass', bg_type, os.path.basename(src_dir))
+            if not os.path.isdir(os.path.split(pass_dir)[0]):
+                os.makedirs(os.path.split(pass_dir)[0])
+            try:
+                os.system("cp {} {}".format(src_dir, pass_dir))
+                log['src_save_dir'].append(pass_dir)
+            except:
+                log['src_save_dir'].append("cannot cp "+src_dir)
+            
+            # save log
+            if save_log_args :
+                with open("log", "wb") as file :
+                    pickle5.dump(log, file)
+
+# main
+perform_augmentation()
